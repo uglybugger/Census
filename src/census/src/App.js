1f0dc857dabe58c6a0e6e17f19a95e57c97fc9d1
Andrew Harcourt <andrewh@uglybugger.org>
2018-11-14 15:29:51 +1000

    Replace logo

--- src/census/src/App.js
+++ src/census/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import logo from './logo.png'   // https://pixabay.com/p-294173/?no_redirect
+import logo from './logo.png';
 import { Grid, Row, Col, Image, Jumbotron } from 'react-bootstrap';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CensusDataEntryPage from './components/pages/CensusDataEntryPage';
@@ -21,9 +21,8 @@ class App extends Component {
                             <Grid>
                                 <Row>
                                     <Col sm={8}>
-                                        <h1>Hipster Census 2018</h1>
-                                        <p>Welcome to the Great Hipster Census of 2018.</p>
-                                        <p>Of course, you knew about it <em>before</em> it was cool.</p>
+                                        <h1>The Great Hipster Census of 2018</h1>
+                                        <p>Of course you knew about it <em>before</em> it was cool.</p>
                                     </Col>
                                     <Col sm={4}>
                                         <Image src={logo} responsive />
